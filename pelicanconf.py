0cac96623e3b26c558e2d71fad0784fd0165f703
Parthiban <31817166+parthiruth@users.noreply.github.com>
2026-04-18 20:42:57 +0530

    Fix formatting in pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -13,7 +13,7 @@ LINKS = (
 # Social widget
 SOCIAL = (
     ("GitHub", "https://github.com/parthiruth/"),
-    ("LinkedIn", "https://www.linkedin.com/in/parthiban-m-407491147"),
+    ("LinkedIn", "https://www.linkedin.com/in/parthiban-m-407491147")
 )
 
 ######## Advanced Settings (not recommended to to edited; take your own risk to touch the settings below) #############
